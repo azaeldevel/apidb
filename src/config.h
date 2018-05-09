6245cbc30a19bbb889af32610698cf6965577cc1
Azael Reyes <azael.rmartz@gmail.com>
2018-05-09 10:30:24 -0700

    [cleaning]

--- src/config.h
+++
@@ -1,5 +0,0 @@
-#define VERSION_MAJOR 0
-#define VERSION_MINOR 1
-#define VERSION_PATCH 0
-#define VERSION_STAGE toolkit::Version::Stage::alpha
-#define PAKAGENAME "apidb"
--- src/generator-sql/CMakeFiles/apidb-generator-sql.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libapidb-generator-sql.a"
-)
