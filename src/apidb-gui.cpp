7926891968e918d7e50e2b1323a52eb8aa3d50d1
Azael R <azael.rmartz@gmail.com>
2019-07-17 16:26:51 -0700

    APIDB GUI
    Las secciones de la ventana se han puesto dentro de funcione [refatory]
    
    Se agrego la seccion de informacion general

--- src/apidb-gui.cpp
+++ src/apidb-gui.cpp
@@ -20,29 +20,24 @@
 
 #include <gtk/gtk.h>
 
-static void print_hello (GtkWidget *widget, gpointer   data)
-{
-        g_print ("Hello World\n");
-}
 
-int main (int   argc, char *argv[])
-{        
-        gtk_init (&argc, &argv);
-        GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+void createWindow(GtkWidget* vboxMain,GtkWidget *window)
+{
         gtk_window_set_title (GTK_WINDOW (window), "APIDB");
         gtk_window_set_default_size (GTK_WINDOW (window), 450, 600);
         g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);  
-        gtk_container_set_border_width (GTK_CONTAINER (window), 10);
-        
-        GtkWidget *vboxMain = gtk_box_new (GTK_ORIENTATION_VERTICAL,2);
-        gtk_container_add (GTK_CONTAINER (window), vboxMain);
-        
-        GtkWidget * toolbar = gtk_toolbar_new();
+        gtk_container_set_border_width (GTK_CONTAINER (window), 10);        
+        gtk_container_add (GTK_CONTAINER (window), vboxMain);        
+}
+void createToolbar(GtkWidget* vboxMain,GtkWidget* toolbar)
+{
         gtk_toolbar_set_style(GTK_TOOLBAR(toolbar), GTK_TOOLBAR_ICONS);
         gtk_box_pack_start(GTK_BOX(vboxMain), toolbar, FALSE, FALSE,0);
+        GtkToolItem *open = gtk_tool_button_new_from_stock(GTK_STOCK_DIRECTORY);
+        gtk_toolbar_insert(GTK_TOOLBAR(toolbar), open, -1);
         GtkToolItem *build = gtk_tool_button_new_from_stock(GTK_STOCK_EXECUTE);
         gtk_toolbar_insert(GTK_TOOLBAR(toolbar), build, -1);
-        GtkToolItem *save = gtk_tool_button_new_from_stock(GTK_STOCK_SAVE);
+        GtkToolItem *save = gtk_tool_button_new_from_stock(GTK_STOCK_HARDDISK);
         gtk_toolbar_insert(GTK_TOOLBAR(toolbar), save, -1);
         GtkToolItem *sep = gtk_separator_tool_item_new();
         gtk_toolbar_insert(GTK_TOOLBAR(toolbar), sep, -1); 
@@ -50,26 +45,95 @@ int main (int   argc, char *argv[])
         g_signal_connect(G_OBJECT(exit), "clicked", G_CALLBACK(gtk_main_quit), NULL);
         gtk_toolbar_insert(GTK_TOOLBAR(toolbar), exit, -1);
         gtk_toolbar_set_icon_size(GTK_TOOLBAR(toolbar),GTK_ICON_SIZE_LARGE_TOOLBAR);
-        //
-        gtk_box_pack_start(GTK_BOX(vboxMain), toolbar, FALSE, FALSE,0);        
+        gtk_box_pack_start(GTK_BOX(vboxMain), toolbar, FALSE, FALSE,0);          
+}
+
+
+void createNotebookInfo(GtkWidget *boxInfo)
+{
+        GtkWidget *boxName = gtk_box_new (GTK_ORIENTATION_HORIZONTAL,2);
+        GtkWidget * lbName = gtk_label_new ("Nombre:");
+        gtk_box_pack_start(GTK_BOX(boxName), lbName, FALSE, FALSE,0); 
+        GtkWidget *inName = gtk_entry_new();
+        gtk_box_pack_start(GTK_BOX(boxName), inName, FALSE, FALSE,0);   
+        gtk_box_pack_start(GTK_BOX(boxInfo), boxName, FALSE, FALSE,0);
         
-        //button = gtk_button_new_with_label ("Button 2");
-        //g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-        //gtk_box_pack_start(GTK_BOX(vboxMain), button, TRUE, TRUE,3);        
+        GtkWidget *boxVer = gtk_box_new (GTK_ORIENTATION_HORIZONTAL,2);
+        GtkWidget * lbVer = gtk_label_new ("Version: ");
+        gtk_box_pack_start(GTK_BOX(boxVer), lbVer, FALSE, FALSE,0); 
+        GtkWidget *inVer = gtk_entry_new();
+        gtk_box_pack_start(GTK_BOX(boxVer), inVer, FALSE, FALSE,0);   
+        gtk_box_pack_start(GTK_BOX(boxInfo), boxVer, FALSE, FALSE,0);
+        
+        GtkWidget *boxInL = gtk_box_new (GTK_ORIENTATION_HORIZONTAL,2);
+        GtkWidget * lbInL = gtk_label_new ("Lenguaje de Entrada:");
+        gtk_box_pack_start(GTK_BOX(boxInL), lbInL, FALSE, FALSE,0); 
+        GtkWidget *inInL = gtk_combo_box_text_new();
+        gtk_combo_box_text_insert((GtkComboBoxText*)inInL,1,"mysql","MySQL");
+        gtk_box_pack_start(GTK_BOX(boxInL), inInL, FALSE, FALSE,0);   
+        gtk_box_pack_start(GTK_BOX(boxInfo), boxInL, FALSE, FALSE,0);
+        
+        GtkWidget *boxOutL = gtk_box_new (GTK_ORIENTATION_HORIZONTAL,2);
+        GtkWidget * lbOutL = gtk_label_new ("Lenguaje de Salida:   ");
+        gtk_box_pack_start(GTK_BOX(boxOutL), lbOutL, FALSE, FALSE,0); 
+        GtkWidget *inOutL = gtk_combo_box_text_new();
+        gtk_combo_box_text_insert((GtkComboBoxText*)inOutL,1,"c++","C++");
+        gtk_box_pack_start(GTK_BOX(boxOutL), inOutL, FALSE, FALSE,0);   
+        gtk_box_pack_start(GTK_BOX(boxInfo), boxOutL, FALSE, FALSE,0);        
+        
+        GtkWidget *boxPkL = gtk_box_new (GTK_ORIENTATION_HORIZONTAL,2);
+        GtkWidget * lbPkL = gtk_label_new ("Empaquetado:   ");
+        gtk_box_pack_start(GTK_BOX(boxPkL), lbPkL, FALSE, FALSE,0); 
+        GtkWidget *inPkL = gtk_combo_box_text_new();
+        gtk_combo_box_text_insert((GtkComboBoxText*)inPkL,1,"cmake","CMake");
+        gtk_box_pack_start(GTK_BOX(boxPkL), inPkL, FALSE, FALSE,0);   
+        gtk_box_pack_start(GTK_BOX(boxInfo), boxPkL, FALSE, FALSE,0);  
+        
+        GtkWidget *boxCmpl = gtk_box_new (GTK_ORIENTATION_HORIZONTAL,2);
+        GtkWidget * lbCmpl = gtk_label_new ("Compilado:        ");
+        gtk_box_pack_start(GTK_BOX(boxCmpl), lbCmpl, FALSE, FALSE,0); 
+        GtkWidget *inCmpl = gtk_combo_box_text_new();
+        gtk_combo_box_text_insert((GtkComboBoxText*)inCmpl,1,"static","Estatico");
+        gtk_box_pack_start(GTK_BOX(boxCmpl), inCmpl, FALSE, FALSE,0);   
+        gtk_box_pack_start(GTK_BOX(boxInfo), boxCmpl, FALSE, FALSE,0);        
+}
+
+void createNotebook(GtkWidget* vboxMain)
+{
         GtkWidget * notebookMain = gtk_notebook_new();
         gchar* strInfo = "Información";
-        GtkWidget *fraInfo = gtk_frame_new("");
-        GtkWidget * lbInfo = gtk_label_new (strInfo);
-        gtk_notebook_append_page (GTK_NOTEBOOK (notebookMain),fraInfo,lbInfo);
-        gtk_box_pack_start(GTK_BOX(vboxMain), notebookMain, TRUE, TRUE,3);
+        GtkWidget *boxInfo = gtk_box_new (GTK_ORIENTATION_VERTICAL,6);
+        GtkWidget *lbInfo = gtk_label_new (strInfo);
+        gtk_notebook_append_page (GTK_NOTEBOOK (notebookMain),boxInfo,lbInfo);
+        createNotebookInfo(boxInfo);
         gchar* strConex = "Conexión";
-        GtkWidget *fraConex = gtk_frame_new("");
+        GtkWidget *boxConex = gtk_box_new (GTK_ORIENTATION_VERTICAL,2);
         GtkWidget * lbConex = gtk_label_new (strConex);
-        gtk_notebook_append_page (GTK_NOTEBOOK (notebookMain),fraConex,lbConex);
-        //
+        gtk_notebook_append_page (GTK_NOTEBOOK (notebookMain),boxConex,lbConex);
+        gchar* strDowns = "Descargas";
+        GtkWidget *boxDowns = gtk_box_new (GTK_ORIENTATION_VERTICAL,2);
+        GtkWidget * lbDowns = gtk_label_new (strDowns);
+        gtk_notebook_append_page (GTK_NOTEBOOK (notebookMain),boxDowns,lbDowns);
+        gchar* strSels = "Selecciones";
+        GtkWidget *boxSels = gtk_box_new (GTK_ORIENTATION_VERTICAL,2);
+        GtkWidget * lbSels = gtk_label_new (strSels);
+        gtk_notebook_append_page (GTK_NOTEBOOK (notebookMain),boxSels,lbSels);
         gtk_box_pack_start(GTK_BOX(vboxMain), notebookMain, FALSE, FALSE,0);
+}
+
+
+int main (int   argc, char *argv[])
+{        
+        gtk_init (&argc, &argv);
+        GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+        GtkWidget *vboxMain = gtk_box_new (GTK_ORIENTATION_VERTICAL,2);
+        createWindow(vboxMain,window);        
         
+        GtkWidget * toolbar = gtk_toolbar_new();
+        createToolbar(vboxMain,toolbar);             
         
+        createNotebook(vboxMain);      
+                
         gtk_widget_show_all (window);
         gtk_main ();
         
