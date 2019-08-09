002f5e787e1b2a4cb2ac9051a207ebe0567052fb
Azael R <azael.devel@gmail.com>
2019-08-09 00:57:12 -0700

    Correcion en error de lectura de las funciones selects y download

--- src/generator-c++.cpp
+++ src/generator-c++.cpp
@@ -194,17 +194,12 @@ namespace generators
                         }
                     }
                     ofile << ";" << std::endl;
-                    ofile << "\t\tif(connector.query(sqlString.c_str()))"  << std::endl;
+                        ofile << "\t\toctetos::toolkit::clientdb::Datresult* dt = connector.query(sqlString.c_str());"  << std::endl;
+                        ofile << "\t\tif(dt!=NULL)"  << std::endl;
                     ofile << "\t\t{" << std::endl;
-                    ofile << "\t\t\tMYSQL_RES *result = mysql_store_result((MYSQL*)connector.getServerConnector());" << std::endl;
-                    ofile << "\t\t\tif (result == NULL)"  << std::endl;
-                    ofile << "\t\t\t{"  << std::endl;
-                    ofile << "\t\t\t\t//throw octetos::toolkit::clientdb::SQLException(\"La descarga de los datos fallo con la consulta '\" + sqlString + \"'\");"<< std::endl;
-                    ofile << "\t\t\t\treturn NULL;"<< std::endl;
-                    ofile << "\t\t\t}"  << std::endl;
                     ofile << "\t\t\tstd::vector<"<< table.getName() << "*>* tmpVc = new std::vector<" << table.getName() << "*>;" << std::endl;
                     ofile << "\t\t\tMYSQL_ROW row;"<< std::endl;
-                    ofile << "\t\t\twhile((row = mysql_fetch_row(result))) "<< std::endl;
+                    ofile << "\t\t\twhile((row = mysql_fetch_row((MYSQL_RES*)(dt->getResult()))))"<< std::endl;
                     ofile << "\t\t\t{"<< std::endl;     
                     ofile << "\t\t\t\t"<< table.getName() << "* tmp = NULL;" << std::endl;             
                     ofile << "\t\t\t\ttmp = new " << table.getName() << "(";              
