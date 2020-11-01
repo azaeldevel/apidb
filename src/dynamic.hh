f1ba32dddcde7862c4360704033560223430233c
Azael Reyes <azael.devel@gmail.com>
2020-11-01 12:40:55 -0800

    fixing: fragmanentation of octetos-db

--- src/dynamic.hh
+++ src/dynamic.hh
@@ -1,3 +1,6 @@
+#ifndef APIDB_DINAMIC_HH
+#define APIDB_DINAMIC_HH
+
 
 #include <octetos/db/clientdb.hh>
 
@@ -7,3 +10,5 @@ extern "C" void destroyConector(octetos::db::Connector*);
 
 extern "C" octetos::db::Datconnect* createDatconnect();
 extern "C" void destroyDatconnect(octetos::db::Datconnect*);
+
+#endif
